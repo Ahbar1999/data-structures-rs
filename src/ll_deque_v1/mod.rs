8b2483a57537a1519319e6c0987b4c7c231460f7
Ahbar1999 <ahbar1234@gmail.com>
2026-03-10 01:33:08 +0530

    added some methods for deque_v1 implementation

--- src/ll_deque_v1/mod.rs
+++ src/ll_deque_v1/mod.rs
@@ -41,6 +41,8 @@ pub mod ll_deque_v1 {
                     // set the next of new head to the old head(which was taken out) 
                     new_node.borrow_mut().next = Some(node.clone());
                     // set the prev of old head to new head
+                    // p.s.: we could have also clone new_node here but since there are no more
+                    // uses to it further down this block we can safely move this value 
                     node.borrow_mut().prev = Some(new_node);
                 },
                 None => {
@@ -50,5 +52,54 @@ pub mod ll_deque_v1 {
                 }
             }
         }
+
+        pub fn pop_front(&mut self) -> Option<T> {
+            self.head.take().map(|old_head| {
+                if let Some(new_head) = old_head.borrow_mut().next.take() {
+                    new_head.borrow_mut().prev.take();   // deinitialize prev pointer 
+
+                    self.head = Some(new_head);
+                } else {
+                    // single node case; deinit tail pointer
+                    self.tail.take();
+                }
+                    
+                Rc::try_unwrap(old_head).ok().unwrap().into_inner().val
+            }) 
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::ll_deque_v1::ll_deque_v1::List;
+
+    #[test]
+    fn basics() {
+        let mut list = List::new();
+
+        // Check empty list behaves right
+        assert_eq!(list.pop_front(), None);
+
+        // Populate list
+        list.push_front(1);
+        list.push_front(2);
+        list.push_front(3);
+
+        // Check normal removal
+        assert_eq!(list.pop_front(), Some(3));
+        assert_eq!(list.pop_front(), Some(2));
+
+        // Push some more just to make sure nothing's corrupted
+        list.push_front(4);
+        list.push_front(5);
+
+        // Check normal removal
+        assert_eq!(list.pop_front(), Some(5));
+        assert_eq!(list.pop_front(), Some(4));
+
+        // Check exhaustion
+        assert_eq!(list.pop_front(), Some(1));
+        assert_eq!(list.pop_front(), None);
     }
 }
