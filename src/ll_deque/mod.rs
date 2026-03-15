ad3ac9e1cf44e8b29a9d715905d4310a599e5d30
Ahbar <ahbar1234@gmail.com>
2026-03-15 23:51:34 +0530

    working on unsafe deque

--- src/ll_deque/mod.rs
+++ src/ll_deque/mod.rs
@@ -1,9 +1,11 @@
 pub mod ll_deque {
+    use std::ptr;
+
     type Link<T> = Option<Box<Node<T>>>;   
 
-    pub struct List<'a, T> {
+    pub struct List<T> {
         head: Link<T>,
-        tail: Option<&'a mut Node<T>>,
+        tail: *mut Node<T>
     } 
 
     pub struct Node<T> {
@@ -17,30 +19,77 @@ pub mod ll_deque {
         }
     }
 
-    impl<'a, T> List<'a, T> {
-        pub fn new() -> List<'a, T> {
-            List{head: None, tail: None}
+    impl<T> List<T> {
+        pub fn new() -> List<T> {
+            List{head: None, tail: ptr::null_mut() }
         }
         
-        pub fn push(&'a mut self, val: T) {
-            let new_tail = Box::new(Node::new(val));
+        // forward storing references, two nodes in sequence: a -> b; 
+        // b cannot be destroyed as long as a has a reference to it
+        pub fn push(&mut self, val: T) {
+            let mut new_tail = Box::new(Node::new(val));
+            let raw_tail: *mut _ = &mut *new_tail; 
             
-            // add the new node at the end of list
-            self.tail = match self.tail.take() {
-                Some(old_tail) => {
-                    // associate(move) newly created to a field of List 
-                    old_tail.next = Some(new_tail);
-                    
-                    // return the reference to the that field(that reference now lives as long as
-                    // the container) 
-                    old_tail.next.as_deref_mut() 
-                },
-                None => {
-                    self.head = Some(new_tail);
-                    
-                    self.head.as_deref_mut()
+            if !self.tail.is_null() {
+                unsafe {
+                    (*self.tail).next = Some(new_tail);  
                 }
+            } else {
+                // empty list
+                self.head = Some(new_tail);
             }
+           
+            self.tail = raw_tail;
         }
+        
+        // pop from front
+        pub fn pop(&mut self) -> Option<T> {
+            self.head.take().map(|old_head| {
+                self.head = old_head.next;
+
+                if self.head.is_none() {
+                    // update self.tail also
+                    self.tail = ptr::null_mut();
+                }
+
+                old_head.val
+            })
+        } 
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::ll_deque::ll_deque::List;
+    #[test]
+    fn basics() {
+        let mut list = List::new();
+
+        // Check empty list behaves right
+        // assert_eq!(list.pop(), None);
+
+        // Populate list
+        list.push(1);
+        list.push(2);   // the scond time you cannot create a mutable borrow as there exists a
+                        // immutable reference in the list itself which peresists for as long as
+                        // the list 
+
+        list.push(3);
+
+        // Check normal removal
+        assert_eq!(list.pop(), Some(1));
+        assert_eq!(list.pop(), Some(2));
+
+        // Push some more just to make sure nothing's corrupted
+        list.push(4);
+        list.push(5);
+
+        // Check normal removal
+        assert_eq!(list.pop(), Some(3));
+        assert_eq!(list.pop(), Some(4));
+
+        // Check exhaustion
+        assert_eq!(list.pop(), Some(5));
+        assert_eq!(list.pop(), None);
     }
 }
