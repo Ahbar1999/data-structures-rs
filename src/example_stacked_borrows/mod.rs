236fee874a4f6cc4619ff1cda9beb80062c86c6c
Ahbar <ahbar1234@gmail.com>
2026-03-17 21:06:51 +0530

    added stacked borrows tests on arrays

--- src/example_stacked_borrows/mod.rs
+++ src/example_stacked_borrows/mod.rs
@@ -35,7 +35,78 @@ pub mod example_stacked_borrows {
             println!("{}", data);
         }
     }
+
+    pub fn snippet_diff_borrows() { // gets caught as UB in miri
+        unsafe {
+            let mut data = [0; 10];
+            let ref1_at_0 = &mut data[0];           // Reference to 0th element
+            let ptr2_at_0 = ref1_at_0 as *mut i32;  // Ptr to 0th element
+            
+
+            let ptr3_at_0 = ptr2_at_0.add(1);       // Ptr to 1st element            
+            // problem: ref1_at_0 only has borrow to the first element of data;
+            // we need two separate mutable borrows to a single allocation of data; and we
+            // cant just do &mut data[i]  
+            // solution: use split_at_mut() on a slice
+            
+            // even with stacked access, its an issue
+            *ptr3_at_0 += 3;
+            *ptr2_at_0 += 2;
+            *ref1_at_0 += 1;
+
+            // Should be [20, 0, 0, ...]
+            println!("{:?}", &data[..]);
+        }
+    }
+
+    pub fn snippet_same_borrrows() {
+        unsafe {
+            let mut data = [0; 10];
+            let ref1_at_0 = &mut data[0];            // Reference to 0th element
+            let ptr2_at_0 = ref1_at_0 as *mut i32;   // Ptr to 0th element
+            let ptr3_at_0 = ptr2_at_0;               // Ptr to 0th element
+            let ptr4_at_0 = ptr2_at_0.add(0);        // Ptr to 0th element
+            let ptr5_at_0 = ptr3_at_0.add(1).sub(1); // Ptr to 0th element
+
+            // An absolute jumbled hash of ptr usages
+            *ptr3_at_0 += 3;
+            *ptr2_at_0 += 2;
+            *ptr4_at_0 += 4;
+            *ptr5_at_0 += 5;
+            *ptr3_at_0 += 3;
+            *ptr2_at_0 += 2;
+            *ref1_at_0 += 1;
+
+            // Should be [20, 0, 0, ...]
+            println!("{:?}", &data[..]);
+        }
+    }
+        
+
+    pub fn snippet_split_borrows() {
+        unsafe {
+            let mut data =  [0; 10];
+
+            let slice1 = &mut data[..]; // create a slice of data
+
+            let (slice2_at_0, slice3_at_1) = slice1.split_at_mut(1);
+
+            let ref4_at_0 = &mut slice2_at_0[0];
+            let ref5_at_1 = &mut slice3_at_1[0];
+            let ptr6_at_0 = ref4_at_0 as *mut i32;
+            let ptr7_at_1 = ref5_at_1 as *mut i32;
+            
+            // still need to follow the stack order
+            *ptr7_at_1 += 7;
+            *ptr6_at_0 += 6;
+            *ref5_at_1 += 5;
+            *ref4_at_0 += 4;
+
+            println!("{:?}", &data[..]);
+       }
+    }
 }
+
 
 #[cfg(test)]
 mod test {
@@ -45,9 +116,23 @@ mod test {
     fn test_ok() {
         snippet_ok();
     }
-    
+   
+    /*
     #[test]
     fn test_err() {
         snippet_err();
     }
+    */
+    
+    #[test]
+    fn test_same_borrow () {
+        snippet_same_borrrows();
+    }
+    
+    /*
+    #[test]
+    fn test_different_borrow () {
+        snippet_diff_borrows();
+    }
+    */
 }
