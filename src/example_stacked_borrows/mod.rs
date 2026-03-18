1ce90315dc3bce30f79862651d6d2c807993adcc
Ahbar <ahbar1234@gmail.com>
2026-03-18 17:50:43 +0530

    working on raw pointers based linked list

--- src/example_stacked_borrows/mod.rs
+++ src/example_stacked_borrows/mod.rs
@@ -105,8 +105,41 @@ pub mod example_stacked_borrows {
             println!("{:?}", &data[..]);
        }
     }
-}
 
+    pub fn snippet_range_borrows() {
+        unsafe {
+            let mut data = [0; 10];
+
+            let slice1_all = &mut data[..];         // Slice for the entire array
+            let ptr2_all = slice1_all.as_mut_ptr(); // Pointer for the entire array
+            
+            let ptr3_at_0 = ptr2_all;               // Pointer to 0th elem (the same)
+            // now this is okay because ptr2_all is a pointer to the whole data not just data[0] 
+            let ptr4_at_1 = ptr2_all.add(1);        // Pointer to 1th elem
+            let ref5_at_0 = &mut *ptr3_at_0;        // Reference to 0th elem
+            let ref6_at_1 = &mut *ptr4_at_1;        // Reference to 1th elem
+
+            *ref6_at_1 += 6;
+            *ref5_at_0 += 5;
+            *ptr4_at_1 += 4;
+            *ptr3_at_0 += 3;
+
+            // Just for fun, modify all the elements in a loop
+            // (Could use any of the raw pointers for this, they share a borrow!)
+            for idx in 0..10 {
+                *ptr2_all.add(idx) += idx;
+            }
+
+            // Safe version of this same code for fun
+            for (idx, elem_ref) in slice1_all.iter_mut().enumerate() {
+                *elem_ref += idx; 
+            }
+
+            // Should be [8, 12, 4, 6, 8, 10, 12, 14, 16, 18]
+            println!("{:?}", &data[..]);
+        }
+    }
+}
 
 #[cfg(test)]
 mod test {
--- src/ll_deque/mod.rs
+++ src/ll_deque/mod.rs
@@ -1,11 +1,12 @@
 pub mod ll_deque {
     use std::ptr;
 
-    type Link<T> = Option<Box<Node<T>>>;   
+    // type Link<T> = Option<Box<Node<T>>>;
+    type Link<T> = *mut Node<T>;    // revised version
 
     pub struct List<T> {
         head: Link<T>,
-        tail: *mut Node<T>
+        tail: Link<T> 
     } 
 
     pub struct Node<T> {
@@ -15,46 +16,63 @@ pub mod ll_deque {
 
     impl<T> Node<T> {
         pub fn new(val: T) -> Node<T> {
-            Node{val, next: None}
+            Node{val, next: ptr::null_mut()}
         }
     }
 
     impl<T> List<T> {
         pub fn new() -> List<T> {
-            List{head: None, tail: ptr::null_mut() }
+            List{head: ptr::null_mut(), tail: ptr::null_mut() }
         }
         
-        // forward storing references, two nodes in sequence: a -> b; 
+        // Old implementation: forward storing references, two nodes in sequence: a -> b; 
         // b cannot be destroyed as long as a has a reference to it
+        
+        // New implmentation: just use raw pointers
         pub fn push(&mut self, val: T) {
-            let mut new_tail = Box::new(Node::new(val));
-            let raw_tail: *mut _ = &mut *new_tail; 
-            
-            if !self.tail.is_null() {
-                unsafe {
-                    (*self.tail).next = Some(new_tail);  
+            unsafe {
+                // essentially a way to get raw pointer malloc style  
+                let new_tail = Box::into_raw(Box::new(Node{
+                    val,
+                    next: ptr::null_mut(),
+                }));
+
+                if !self.tail.is_null() {
+                    (*self.tail).next = new_tail;
+                } else {
+                    self.head = new_tail;
                 }
-            } else {
-                // empty list
-                self.head = Some(new_tail);
-            }
-           
-            self.tail = raw_tail;
+
+                self.tail = new_tail;
+            } 
         }
         
         // pop from front
         pub fn pop(&mut self) -> Option<T> {
-            self.head.take().map(|old_head| {
-                self.head = old_head.next;
+            unsafe {
+                if self.head.is_null() {
+                    None 
+                } else {
+                    let old_head = Box::from_raw(self.head);
+                    self.head = old_head.next;
 
-                if self.head.is_none() {
-                    // update self.tail also
-                    self.tail = ptr::null_mut();
+                    if self.head.is_null() {
+                        self.tail = ptr::null_mut();
+                    }
+                    
+                    // move the val here
+                    Some(old_head.val)
                 }
+            }
 
-                old_head.val
-            })
+            // old_head dropped here
         } 
+    }
+
+    impl<T> Drop for List<T> {
+        fn drop(&mut self) {
+            while let Some(_) = self.pop() { }
+        }
     }
 }
 
@@ -67,7 +85,7 @@ mod test {
         let mut list = List::new();
 
         // Check empty list behaves right
-        // assert_eq!(list.pop(), None);
+        assert_eq!(list.pop(), None);
 
         // Populate list
         list.push(1);
@@ -91,6 +109,15 @@ mod test {
 
         // Check exhaustion
         assert_eq!(list.pop(), Some(5));
+        assert_eq!(list.pop(), None);
+
+        // Check the exhaustion case fixed the pointer right
+        list.push(6);
+        list.push(7);
+
+        // Check normal removal
+        assert_eq!(list.pop(), Some(6));
+        assert_eq!(list.pop(), Some(7));
         assert_eq!(list.pop(), None);
     }
 }
